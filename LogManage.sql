c4fa3d7d2d7ae70f3d6ca9626d90c20baf97e8c5
Lacishtar <truonglac1905@gmail.com>
2023-05-10 23:13:02 +0700

    Software Engineer

---
+++ LogManage.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE LogManage;
+
+USE LogManage;
+
+CREATE TABLE Log (
+    UserName VARCHAR(30),
+    Password VARCHAR(30)
+);
+
+INSERT INTO Log VALUES ('truonglac1905', '12345');
+INSERT INTO Log VALUES ('chanchongkhoa', '654321');
+INSERT INTO Log VALUES ('admin', 'tdtu');
---
+++ Readme.txt
@@ -0,0 +1,11 @@
+Link demo youtube: https://www.youtube.com/watch?v=WT3Cx9HDwCA
+
+There are 3 available account:
+username: "admin"
+password" "tdtu"
+
+username: "truonglac1905"
+password" "123456"
+
+username: "chanchongkhoa"
+password" "654321"
